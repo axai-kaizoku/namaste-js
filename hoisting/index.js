ec24e97d1ddec15a54e972bf6c183be935254961
Akshay Yelle <02b3akshay@gmail.com>
2025-06-23 10:38:26 +0530

    add hoisting

---
+++ hoisting/index.js
@@ -0,0 +1,26 @@
+
+
+// getName()
+// console.log(x);
+// console.log(getName)
+
+
+var x = 7;
+
+
+// var getName2 = function () {
+//     console.log("Namaste Javascript")
+// }
+//
+// var getName = () => {
+//     console.log("Namaste Javascript")
+// }
+
+
+function getName() {
+    console.log("Namaste Javascript")
+}
+
+getName()
+console.log(x);
+console.log(getName)
