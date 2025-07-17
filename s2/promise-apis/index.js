d28adaa7b12710e47022a07fa55ded7e3a4119f6
Akshay Yelle <02b3akshay@gmail.com>
2025-07-17 22:37:22 +0530

    add promises Promise.all Promise.allSettled Promise.race Promise.any

--- s2/promise-apis/index.js
+++ s2/promise-apis/index.js
@@ -1,20 +1,57 @@
-const API = "https://api.github.com/users/axai-kaizoku"
+/**
+ * Promise.all
+ * Promise.allSettled
+ * Promise.race
+ * Promise.any
+ * Promise.resolve
+ * Promise.reject
+ */
 
-let data
-
-function getData(data) {
-  console.log(data)
+function createPromise(ms = 1500, value = 1, isReject = false) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isReject) {
+        reject(value)
+      } else {
+        resolve(value)
+      }
+    }, ms)
+  })
 }
 
-fetch(API)
-  .then((res) => res.json())
-  .then((res) => {
-    // console.log(data)
-    // getData(res)
-    data = res
-  })
+const p1 = createPromise(3000, "p1 Success", true)
+const p2 = createPromise(1000, "p2 Success", true)
+const p3 = createPromise(2000, "p3 Success", true)
 
-setTimeout(() => {
-  console.log(data)
-}, 0)
-// getData()
+// Promise.all([p1, p2, p3])
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+
+// Promise.allSettled([p1, p2, p3])
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+
+// Promise.race([p1, p2, p3])
+//   .then((res) => {
+//     console.log(res)
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+
+Promise.any([p1, p2, p3])
+  .then((res) => {
+    console.log(res)
+  })
+  .catch((err) => {
+    console.error(err)
+    console.log(err.errors)
+  })
