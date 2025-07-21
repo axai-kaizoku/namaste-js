0bcc4725afb17f54c30728ffe58747f9e86dbed7
Akshay Yelle <02b3akshay@gmail.com>
2025-07-21 23:20:44 +0530

    update promise apis

--- s2/promise-apis/index.js
+++ s2/promise-apis/index.js
@@ -19,33 +19,33 @@ function createPromise(ms = 1500, value = 1, isReject = false) {
   })
 }
 
-const p1 = createPromise(3000, "p1 Success", true)
-const p2 = createPromise(1000, "p2 Success", true)
-const p3 = createPromise(2000, "p3 Success", true)
+const p1 = createPromise(3000, "p1 Success")
+const p2 = createPromise(1000, "p2 Success")
+const p3 = createPromise(2000, "p3 Success")
 
 // Promise.all([p1, p2, p3])
-//   .then((res) => {
-//     console.log(res)
-//   })
-//   .catch((err) => {
-//     console.error(err)
-//   })
+// 	.then((res) => {
+// 		console.log(res);
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 	});
 
 // Promise.allSettled([p1, p2, p3])
-//   .then((res) => {
-//     console.log(res)
-//   })
-//   .catch((err) => {
-//     console.error(err)
-//   })
+// 	.then((res) => {
+// 		console.log(res);
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 	});
 
 // Promise.race([p1, p2, p3])
-//   .then((res) => {
-//     console.log(res)
-//   })
-//   .catch((err) => {
-//     console.error(err)
-//   })
+// 	.then((res) => {
+// 		console.log(res);
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 	});
 
 Promise.any([p1, p2, p3])
   .then((res) => {
